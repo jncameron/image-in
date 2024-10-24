bd796fc85d0a9408b006531970a1842111960e46
John Cameron <john.n.cameron@gmail.com>
2024-10-24 21:54:24 +1000

    basic routing

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Root = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="root">
+      <div className="root-container">
+        <div className="wrapper">{children}</div>
+      </div>
+    </main>
+  );
+};
+
+export default Root;
